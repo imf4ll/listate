580223cfb73d7e9202fb7457147220a2518d1cd7
z3oxs <z3ox1s@protonmail.com>
2022-04-20 07:00:58 -0300

    First commit | v1.0.0

---
+++ src/themes/index.js
@@ -0,0 +1,31 @@
+export const dark = {
+    primary: '#eee',
+    secondary: '#121212',
+    text: '#fff',
+    background: '#202020',
+    Template: {
+        border: '#404040',
+    },
+    Menu: {
+        icon: '#8a8a8a',
+    },
+    Header: {
+        button: 'rgb(120, 120, 120)',
+    }
+}
+
+export const light = {
+    primary: '#151515',
+    secondary: '#eee',
+    text: '#101010',
+    background: 'rgb(225, 225, 225)',
+    Template: {
+        border: 'rgb(200, 200, 200)',
+    },
+    Menu: {
+        icon: 'rgb(50, 50, 50)',
+    },
+    Header: {
+        button: 'rgb(100, 100, 100)',
+    }
+}
